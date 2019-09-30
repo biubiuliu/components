34b66ee9b81c20b994544ad2b66921df9ae4bc54
biubiuliu <172117859@qq.com>
2019-09-30 17:39:11 +0800

    '布局'

---
+++ softdesign/src/components/canvas/canvasCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="canvas_container">画布</div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+.canvas_container {
+  width: 100%;
+  height: 200px;
+  background: antiquewhite;
+}
+</style>
---
+++ softdesign/src/view/home/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="home_container">
+    <div class="left_box">d</div>
+    <el-container>
+      <el-header>header</el-header>
+      <el-main>
+        <canvas-card />
+      </el-main>
+      <el-footer>footer</el-footer>
+    </el-container>
+  </div>
+</template>
+<script>
+import canvasCard from '@/components/canvas/canvasCard'
+export default {
+  components: {
+    canvasCard
+  },
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+.home_container {
+  display: flex;
+  justify-content: flex-start;
+}
+.el-header {
+  background-color: #b3c0d1;
+  color: #333;
+  line-height: 60px;
+}
+.el-footer {
+  background-color: #b3c0d1;
+  color: #333;
+  line-height: 60px;
+}
+.left_box {
+  background: rgb(214, 182, 182);
+  width: 20%;
+  height: 100vh;
+}
+</style>
